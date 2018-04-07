e160457b6fde8d8dba8bb2f8b110627a80db1b8a
jpr <jan.prochazka@mail.com>
2018-04-07 19:07:04 +0200

    Two new records

---
+++ 3-Simplyfication.py
@@ -0,0 +1,16 @@
+examples=[["a + a = b", "b - d = c", "a + b = d"],
+          ["a + 3g = k", "-70a = g"],
+          ["-j -j -j + j = b"]
+         ]
+formula=["c + a + b",
+         "-k + a",
+         "-j - b"
+        ]
+answer=["2a",
+        "210a",
+        "1j"
+       ]
+
+def simplify(examples,formula):
+
+    return formula[0]
